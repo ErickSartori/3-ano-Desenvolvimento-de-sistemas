2d1f29fa36016b57d8f7b29edd624133cdfbce1b
Erick.Sartori <125655944+ErickSartori@users.noreply.github.com>
2024-02-15 09:13:03 -0300

    Add files via upload

---
+++ back.end/index.js
@@ -0,0 +1,14 @@
+let nome;
+nome = prompt ("Digite o seu nome");
+console.log(nome);
+window.alert('Olá ${nome} !');
+let numero = 19;
+let palpite = prompt ("digite um número de 0 a 20: ");
+console. log(palpite)
+if (numero == palpite) {
+    window.alert(`Parabéns ${nome}! O numero era ${numero}, e voçe acertou!`);
+}
+else {
+    window.alert(`Voçê`)
+}
+
